b3aa1448f3c3b4aff62c74e00f4dcd9719214770
Jojogeek <89109558+pariya-tavangar@users.noreply.github.com>
2024-04-13 00:02:11 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,69 +1,60 @@
-# from pytube import YouTube
-
-# def Download(link):
-#     youtubeObject = YouTube(link)
-#     youtubeObject = youtubeObject.streams.get_highest_resolution()
-#     try:
-#         youtubeObject.download()
-#     except:
-#         print("An error has occurred")
-#     print("Download is completed successfully")
-
-
-# link = input("Enter the YouTube video URL: ")
-# Download(link)
+from pytubefix import YouTube
 from datetime import timedelta
 from telebot import TeleBot
 from telebot import *
 from telebot.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 
-from pytube import YouTube
-import requests
-from bs4 import BeautifulSoup
+bot = TeleBot(token = 'your token')
 
-video = YouTube('https://www.youtu.be/k3CLd-Zc3r0?si=dzeQcGGsgcC5DtXy')
-
-bot = TeleBot(token = 'token')
-
-first_markup = InlineKeyboardMarkup(row_width=2)
-button1 = InlineKeyboardButton(text='240',callback_data='q-240')
-button2 = InlineKeyboardButton(text='320',callback_data='q-320')
-button3 = InlineKeyboardButton(text='480',callback_data='q-480')
-button4 = InlineKeyboardButton(text='720',callback_data='q-720')
-button5 = InlineKeyboardButton(text='1080',callback_data='q-1080')
-first_markup.add(button1,button2,button3,button4,button5)
+first_markup = InlineKeyboardMarkup(row_width=3)
+button1 = InlineKeyboardButton(text='240',callback_data='q-360')
+button2 = InlineKeyboardButton(text='320',callback_data='720')
+button3 = InlineKeyboardButton(text='480',callback_data='1080')
+first_markup.add(button1,button2,button3)
 
 
 @bot.message_handler(commands=['start'])
 def starting(message):
-    bot.send_message(message.chat.id,"Hi send me youtube link !")
-
-
-@bot.message_handler()
-def keyboard(message):
-    if 'https' in message.text:
-        #recieve link
-        bot.send_message(message.chat.id,"Choose your quality",reply_markup=first_markup)         
-    else:
-        bot.send_message(message.chat.id,"It's not a link") 
+    bot.send_message(message.chat.id,"Hi there.. send me any youtube link !")
 
 
 
+@bot.message_handler(func = lambda message : True)
+def handler_text(message):
+
+    global usr_link
+    usr_link = YouTube(message.text)    
+    v_title  = usr_link.title
+    v_author = usr_link.author
+    v_length = usr_link.length
+    v_length = (str(timedelta(seconds = v_length)))
+    # v_des = usr_link.description
+    v_tumb = usr_link.thumbnail_url
+    bot.send_message(message.chat.id,"Your Link Information:\n\n|Title| --> {0}\n|Author| --> {1}\n|Length| --> {2}\n|Tumbnail| --> {3}".format(v_title,v_author,v_length,v_tumb),reply_markup=first_markup)
 
 
-@bot.callback_query_handler(func = lambda call: call.data=='q-240')
-def download_240(call):
-    video.streams.filter(res='240').first().download()
+
+@bot.callback_query_handler(func = lambda call:call.data=="q-360")
+def download_360(call):
+
+    usr_video = usr_link.streams.filter(res="360p").first()
+    usr_video.download('toto-utube-downloader-master/videos')
+
+
+@bot.callback_query_handler(func = lambda call:call.data=="q-720")
+def download_720(call):
+
+    usr_video = usr_link.streams.filter(res="720p").first()
+    usr_video.download('toto-utube-downloader-master/videos')
+
+
+@bot.callback_query_handler(func = lambda call:call.data=="q-1080")
+def download_1080(call):
+
+    usr_video = usr_link.streams.filter(res="1080p").first()
+    usr_video.download('toto-utube-downloader-master/videos')
 
 
 
-# try:
-#     print('Title: ',video.title)
-#     print('Author: ',video.author)
-#     video_length = video.length
-#     print('Lenght: ',(str(timedelta(seconds=video_length))))
-
-# except:
-#     print('ummm')
-
+bot.infinity_polling()
